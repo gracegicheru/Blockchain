b392de37e192ccb037446fb0ff186361c5a7c90e
gracegicheru <gracegicheru3@gmail.com>
2020-08-23 18:05:02 +0300

    changes

--- blockchain.py
+++ blockchain.py
@@ -30,11 +30,12 @@ def add_transaction(recipient, sender= owner, amount= 1.0):
 
 def open_mine():
     last_block= blockchain[-1]
-    hashed_block= ''
-    for key in last_block:
-        value= last_block[key]
-        hashed_block=hashed_block + str(value)
-        print(hashed_block)
+    hashed_block='-'.join([str(last_block[key]) for key in last_block])
+
+    # for key in last_block:
+    #     value= last_block[key]
+    #     hashed_block=hashed_block + str(value)
+    print(hashed_block)
     block= {
         'previous_hash':hashed_block,
         'index': len(blockchain),
